445f3ad44baf6d0402b4f143dae905788e4760f7
z9877969 <z9877969@ukr.net>
2020-08-27 00:36:27 +0300

    first commit -> create react app

---
+++ src/components/Block/Block.js
@@ -0,0 +1,13 @@
+import React from "react";
+import css from './Block.modules.css';
+
+
+const Block = () => {
+    return (
+        <div className={css.wrapper}>
+            <h1 className={css.title}>Hello world!</h1>
+        </div>
+    )
+}
+
+export default  Block;
