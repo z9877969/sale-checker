bf4d26f94ad7141b86ffb0f0e2650ff89e7fec6b
z9877969 <z9877969@ukr.net>
2020-09-25 01:23:44 +0300

    Updates

---
+++ precache-manifest.d6847198d07340799bfa058b5463863a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f5765bd891fdc1fe66e015d630ba1e1",
+    "url": "/sale-checker/index.html"
+  },
+  {
+    "revision": "3851df5a14efcd974267",
+    "url": "/sale-checker/static/css/main.6c1a2dfd.chunk.css"
+  },
+  {
+    "revision": "818a098b15bc9301fb40",
+    "url": "/sale-checker/static/js/2.be7e763e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sale-checker/static/js/2.be7e763e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3851df5a14efcd974267",
+    "url": "/sale-checker/static/js/main.3850039c.chunk.js"
+  },
+  {
+    "revision": "e36928a49476f155bd52",
+    "url": "/sale-checker/static/js/runtime-main.b1f915ff.js"
+  },
+  {
+    "revision": "f8424cd6d28763fc4f05821a07b52b6a",
+    "url": "/sale-checker/static/media/cross.f8424cd6.svg"
+  },
+  {
+    "revision": "178bb4e3e9b0f86cbe7582c7a5302e03",
+    "url": "/sale-checker/static/media/files-empty.178bb4e3.svg"
+  },
+  {
+    "revision": "5ea71b592e72fb2ccf7b473fc40e3c2b",
+    "url": "/sale-checker/static/media/minus.5ea71b59.svg"
+  }
+]);
